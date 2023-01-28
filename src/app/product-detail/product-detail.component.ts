4e41c20e5ae86dcb150ff6b5abbe1f7f32305806
jiaen zhang <jiaen.zhang98@gmail.com>
2023-01-28 09:12:20 +0800

    product detail page

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product';
+import { MOCK_PRODUCTS } from 'src/assets/static/data/mock-products';
 import { ProductService } from '../services/product-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
+  product: Product | undefined;
 
-  // products: Product[] = [];
-  @Input() product?: Product;
-
-  constructor(private productService: ProductService) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = MOCK_PRODUCTS.find(
+      (product) => product.id === productIdFromRoute
+    );
+
     // this.productService.getMainProducts()
     //   .subscribe(products => this.products = products);
     // this.products.forEach(product => console.log(product));
     // console.log("HelloWorld");
   }
-
 }
