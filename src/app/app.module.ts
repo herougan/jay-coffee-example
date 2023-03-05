2182c5be3022342fbde49282ffbb5c7e91708d5c
jiaen zhang <jiaen.zhang98@gmail.com>
2023-03-05 12:43:59 +0800

    added pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -61,11 +62,12 @@ import { AlertModule } from './modules/alert-module/alert-module.module';
   ],
   imports: [
     BrowserModule,
+    NgxPaginationModule,
     AppRoutingModule,
     AlertModule,
     StoreModule.forRoot({ cart: cartReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
