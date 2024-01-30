1778aa1a03e7d49c75b8c1345a54e1158de709a1
Jay Zhang <zhangjiahaor@gmail.com>
2024-01-30 17:40:42 +0800

    Update changes

---
+++ docs/assets/static/data/mock-feature-card-details.ts
@@ -0,0 +1,28 @@
+import { FeatureCardDetail } from 'src/app/models/feature-card-details';
+
+export const MOCK_FEATURE_CARD_DETAILS: FeatureCardDetail[] = [
+  {
+    id: 5,
+    name: 'La Cama',
+    img: 'assets/static/images/products/la_cama.jpg',
+    desc: 'Learn to make cute toy',
+    tags: [''],
+    copy: '3 Hour Course, Make your very own custom plush toy.',
+  },
+  {
+    id: 6,
+    name: 'Fuschia Limelight',
+    img: 'assets/static/images/products/pink_lime_cake.jpg',
+    desc: 'Bring them home',
+    tags: [''],
+    copy: 'Design custom toys online or onsite, and get them delivered, or stay to join in its production!',
+  },
+  {
+    id: 7,
+    name: 'Bloody Mary',
+    img: 'assets/static/images/products/bloody_mary.jpg',
+    desc: 'A bonding experience',
+    tags: [''],
+    copy: 'Great for pimping out your room or office',
+  },
+];
